0cdeb192187a906afe1fa54f063bc747c504a2d7
Yuri Kopshev <yura.kopshev@gmail.com>
2020-07-13 14:06:37 +0200

    file was changed

---
+++ src/Bonus.java
@@ -0,0 +1,7 @@
+public class Bonus {
+    public static void main(String[] args) {
+        double TicketPrice = 99.56;
+        int BonusMiles = (int) (TicketPrice / 20);
+        System.out.println(BonusMiles);
+    }
+}
